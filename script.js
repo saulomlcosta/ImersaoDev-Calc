44cbeaa85933ca9c06912be9b1c2848c471b8d68
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2021-04-12 19:30:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,46 +31,41 @@ else if (operação == 3) {
 
 else if (operação == 4) {
   var primeiroValor = prompt("digite o primeiro valor:")
-var segundoValor = prompt("digite o segundo valor:")
-var primeiroValorNumero = parseInt(primeiroValor)
-var segundoValorNumero = parseInt(segundoValor)
+  var segundoValor = prompt("digite o segundo valor:")
+  var primeiroValorNumero = parseInt(primeiroValor)
+  var segundoValorNumero = parseInt(segundoValor)
   var resultado = primeiroValorNumero - segundoValorNumero
   document.write("<h2>" + primeiroValorNumero + " - " + segundoValor + " = " + resultado + "</h2>")
 }
 
 else if (operação == 5) {
   var numeroComoTexto = prompt("qual o numero que quer colocar a potencia?")
-
-var potencia = prompt("qual a potencia desse numero?")
-
-var numeroComonumero = parseInt(numeroComoTexto)
-
-var potenciaComoNumero = parseInt(potencia)
-
-var resultado = numeroComonumero**potenciaComoNumero
-
-document.write("<h2>" + resultado + "</h2>")
+  var potencia = prompt("qual a potencia desse numero?")
+  var numeroComonumero = parseInt(numeroComoTexto)
+  var potenciaComoNumero = parseInt(potencia)
+  var resultado = numeroComonumero**potenciaComoNumero
+  document.write("<h2>" + resultado + "</h2>")
 }
 
 else if (operação == 6) {
   var entrada, x, valorTotalEntrada, valor, resultado;
-entrada = prompt("Digite o valor total");
-x = parseInt(entrada);
-valorTotalEntrada = prompt("para qual a porcentagem? ");
-valor = parseInt(valorTotalEntrada);
-resultado = valor*(x/100);
-document.write("<h2>" + resultado + "%" + "</h2>")
+  entrada = prompt("Digite o valor total");
+  x = parseInt(entrada);
+  valorTotalEntrada = prompt("para qual a porcentagem? ");
+  valor = parseInt(valorTotalEntrada);
+  resultado = valor*(x/100);
+  document.write("<h2>" + resultado + "%" + "</h2>")
 }
 
 else if (operação == 7) {
   var digito = parseInt(prompt("qual valor quer obter a raiz quadrada?"))
   var resultado = Math.sqrt(digito)
-document.write("<h2>" + resultado + "</h2>")
+  document.write("<h2>" + resultado + "</h2>")
   
 }
 
 else {
-  document.write("<h2>opção invalida</h2>")
+  document.write("<h2>opção invalida</h2>") 
 }
 
 //escrevendo na tela - document.write()
